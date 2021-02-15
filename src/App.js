1bfd5fc9a098bf383885d60baf63d3bdec02ee1e
aaronstaes <aaron.staes@gmail.com>
2021-02-15 11:19:04 +0100

    Added Components  Player.js  Song.js  Added FontAwesome dependecies  Added play-control icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import react from "react";
 
+// Adding components
+import Player from './Components/Player';
+import Song from './Components/Song';
+
 function App() {
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
