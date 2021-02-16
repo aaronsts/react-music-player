3d404ab5e23db1132c0fa6c49b8fad74adf85080
aaronstaes <aaron.staes@gmail.com>
2021-02-16 23:26:41 +0100

    Added Highligthed Song

--- src/Components/LibrarySong.js
+++ src/Components/LibrarySong.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-const LibrarySong = ({song, songs, setCurrentSong, audioRef, isPlaying}) => {
+const LibrarySong = ({song, songs, setCurrentSong, audioRef, isPlaying, setSongs}) => {
     const songSelectHandler = () => {
         setCurrentSong(song);
+        // Add Active State for current song
+        const newSongs = songs.map((selectedSong)=> {
+          if(selectedSong.id === song.id){
+            return{
+              ...selectedSong,
+              active: true,
+            }
+          }else{
+            return{
+              ...selectedSong,
+              active: false,
+            }
+          }
+        });
+        setSongs(newSongs);
         // check if song is playing
         if(isPlaying){
             const playPromise = audioRef.current.play();
@@ -10,12 +25,12 @@ const LibrarySong = ({song, songs, setCurrentSong, audioRef, isPlaying}) => {
                 playPromise.then((audio) => {
                     audioRef.current.play();
                 });
-            }
-            
-        }
+            };
+             
+        };
     };
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
             <img alt={song.name} src={song.cover}></img>
             <div className="song-description">
                 <h3>{song.name}</h3>
